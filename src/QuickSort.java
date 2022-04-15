c7713af322dd299f2ba8d30908b30f70bda255bc
Vladimir Tolstikhin <vr.tn1602@mail.ru>
2022-04-15 20:52:46 +0300

    Небольшое наведение порядка в коде

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,47 +1,49 @@
 import java.util.Map;
 
 public class QuickSort {
+    private static final int COEFF_RATE_IN_MINUTES = 60;
 
     public static void quickSortTreeMap(Map<Integer, Event> eventMemory, int lowKey, int highKey) {
         if (eventMemory.size() == 0)
-            return;//завершить выполнение если длина массива равна 0
+            return; // Завершение выполнения, если длина массива равна 0
 
         if (lowKey >= highKey)
-            return;//завершить выполнение если уже нечего делить
+            return; // Завершение выполнения, если уже нечего делить
 
-        // выбрать опорный элемент
+        // Выбор опорного элемента
         int middle = lowKey + (highKey - lowKey) / 2;
         int opora = spawnTime(middle, eventMemory);
 
-        // разделить на подмассивы, который больше и меньше опорного элемента
-        int i = lowKey, j = highKey;
-        while (i <= j) {
-            while (spawnTime(i, eventMemory) < opora) {
-                i++;
+        // Разделение на подмассивы, который больше и меньше опорного элемента
+        int countLowKey = lowKey, countHighKey = highKey;
+        while (countLowKey <= countHighKey) {
+            while (spawnTime(countLowKey, eventMemory) < opora) {
+                countLowKey++;
             }
 
-            while (spawnTime(j, eventMemory) > opora) {
-                j--;
+            while (spawnTime(countHighKey, eventMemory) > opora) {
+                countHighKey--;
             }
 
-            if (i <= j) {//меняем местами
-                Event temp = eventMemory.get(i);
-                eventMemory.put(i, eventMemory.get(j));
-                eventMemory.put(j, temp);
-                i++;
-                j--;
+            // Меняем местами
+            if (countLowKey <= countHighKey) {
+                Event temp = eventMemory.get(countLowKey);
+                eventMemory.put(countLowKey, eventMemory.get(countHighKey));
+                eventMemory.put(countHighKey, temp);
+                countLowKey++;
+                countHighKey--;
             }
         }
 
-        // вызов рекурсии для сортировки левой и правой части
-        if (lowKey < j)
-            quickSortTreeMap(eventMemory, lowKey, j);
+        // Вызов рекурсии для сортировки левой и правой частей
+        if (lowKey < countHighKey)
+            quickSortTreeMap(eventMemory, lowKey, countHighKey);
 
-        if (highKey > i)
-            quickSortTreeMap(eventMemory, i, highKey);
+        if (highKey > countLowKey)
+            quickSortTreeMap(eventMemory, countLowKey, highKey);
     }
 
     public static int spawnTime(int i, Map<Integer, Event> eventMemory) {
-        return Integer.parseInt(eventMemory.get(i).getEventHours()) * 60 + Integer.parseInt(eventMemory.get(i).getEventMinutes());
+        return Integer.parseInt(eventMemory.get(i).getEventHours()) * COEFF_RATE_IN_MINUTES + Integer.parseInt(eventMemory.get(i).getEventMinutes());
     }
 }
